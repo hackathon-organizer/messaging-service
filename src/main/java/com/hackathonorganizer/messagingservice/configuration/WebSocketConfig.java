c15c6cb59b90e8eeae9caf6a0687c628e284a837
Tegridy <jarjak4@gmail.com>
2022-10-14 21:41:45 +0200

    Fix packages structure

--- src/main/java/com/hackathonorganizer/messagingservice/configuration/WebSocketConfig.java
+++ src/main/java/com/hackathonorganizer/messagingservice/configuration/WebSocketConfig.java
@@ -1,19 +1,10 @@
 package com.hackathonorganizer.messagingservice.configuration;
 
-import com.hackathonorganizer.messagingservice.websocket.SignalHandler;
+import com.hackathonorganizer.messagingservice.websocket.service.SignalHandler;
 import com.hackathonorganizer.messagingservice.websocket.service.MessageService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.messaging.simp.stomp.StompHeaders;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.*;
-import org.springframework.web.socket.server.HandshakeInterceptor;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 @Configuration
 @EnableWebSocket
--- src/main/java/com/hackathonorganizer/messagingservice/websocket/service/SignalHandler.java
+++ src/main/java/com/hackathonorganizer/messagingservice/websocket/service/SignalHandler.java
@@ -1,4 +1,4 @@
-package com.hackathonorganizer.messagingservice.websocket;
+package com.hackathonorganizer.messagingservice.websocket.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
