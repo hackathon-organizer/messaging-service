0602fd4f6968f3756d4d7e83c60096335addedbf
Tegridy <jarjak4@gmail.com>
2023-02-12 09:59:26 +0100

    Update app config and ws origins fetch

--- src/main/java/com/hackathonorganizer/messagingservice/configuration/WebSocketConfig.java
+++ src/main/java/com/hackathonorganizer/messagingservice/configuration/WebSocketConfig.java
@@ -3,6 +3,7 @@ package com.hackathonorganizer.messagingservice.configuration;
 import com.hackathonorganizer.messagingservice.websocket.service.MessageService;
 import com.hackathonorganizer.messagingservice.websocket.service.SignalHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -14,14 +15,16 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
 
     private final MessageService messageService;
-
     private final VaultProperties vaultProperties;
+
+    @Value("${ws.origins}")
+    private String origins;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 
         registry.addHandler(new SignalHandler(vaultProperties, messageService),
                         "/messages-websocket")
-                .setAllowedOrigins("*");
+                .setAllowedOrigins(origins);
     }
 }
